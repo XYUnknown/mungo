b79cb35ff017f1879cf4cfe5c86a0a2a87e1aa74
Xueying Qin <2335466q@student.gla.ac.uk>
2018-07-22 18:55:35 +0100

    Add test cases

---
+++ testfiles/cat/CatLinearValid.java
@@ -0,0 +1,19 @@
+package examples.cat;
+
+class CatLinearValid {
+	public static void main(String [] args) {
+		Cat c1 = new Cat();
+		Cat c2 = new Cat();
+		c1.initialise();
+		c2.initialise();
+		Cat c3 = c2;
+		c1.playWith(c3);
+		c3.poison();
+		Cat c4 = new Cat();
+		c4.initialise();
+		c1.close();
+		c3.bury();
+		c4.close();
+		c2 = new Cat();
+	}
+}
