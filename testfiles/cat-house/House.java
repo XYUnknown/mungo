487eccd77ff7573e7b658da68a8b652fb1b4df60
Xueying Qin <2335466q@student.gla.ac.uk>
2018-08-16 15:33:49 +0100

    Update test cases

--- testfiles/cat-house/House.java
+++ testfiles/cat-house/House.java
@@ -8,14 +8,15 @@ public class House {
 
 	}
 
-	public void createCat() {
-		c = new Cat();
-	}
-
 	public void killCat() {
 		c.giveBirth();
 		c.poison();
 		System.out.println("Your cat is killed");
+	}
+
+	public void createCat() {
+		c = new Cat();
+		c.initialise();
 	}
 
 	public void close() {
